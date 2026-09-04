79db2d14473ef2ad882eae00a5273f434c8d8939
David Tamaratare Oghenebrume <tamaratare.dave@gmail.com>
2026-09-04 17:20:07 +0100

    Update

--- src/Template_Horrors.cpp
+++ src/Template_Horrors.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <functional>
 /**
  * @file Template_Horrors.cpp
  * 
@@ -8,12 +8,26 @@
  * Blah blah blah gang
  */
 
+ /**
+  * @requirement REQ-TEMP-101 Multiple Arguments Function
+  * @brief We need a function that can take any number of arguments
+  * 
+  * How can we do this gang?
+  */
+
 /**
- * @fn void<Lmao...> brochaho( Lmao&&... aValues )
+ * @fn void brochaho( Lmao&&... aValues )
  * 
  * @brief Function can take any number of arguments
  *
  * @param aValues
+ * 
+ * Examples of some data types allowed:
+ *  -# int
+ *  -# float
+ *  -# double
+ * 
+ * @satisfies REQ-TEMP-101 Implements a Function that can take n arguments (n = 1, 2, 3...)
  */
 template <class... Lmao> void brochaho( Lmao&&... aValues ) {
   constexpr std::size_t si = sizeof...(aValues) - 1;
@@ -21,6 +35,27 @@ template <class... Lmao> void brochaho( Lmao&&... aValues ) {
   ((std::cout << "The values are ") << ... << aValues) << std::endl;
 }
 
-int main(){
-    brochaho(3, " ", 4, " ", "Omo");
+/**
+ * @fn void TestMultFunc()
+ * 
+ * @brief I don't know gang
+ * 
+ * Oh boy stand up and be counted
+ * 
+ * @verifies REQ-TEMP-101 Good Boy
+ */
+void TestMultFunc(){
+  std::cout << "Gang" << std::endl;
+}
+
+/**
+ * @brief Just your average global variable
+ * 
+ * Sup homie?
+ */
+const std::function<void()>& TMF = TestMultFunc;
+
+int main()
+{
+  TMF();
 }
