def3f6f02b48a59ac1200434aa14ebd776b2dfcf
David Tamaratare Oghenebrume <tamaratare.dave@gmail.com>
2026-09-14 16:37:10 +0100

    Update

---
+++ src/LearnCMake.cpp
@@ -0,0 +1,12 @@
+#include <LearnCMake.hpp>
+#include <tuple>
+
+static std::tuple<std::string, std::string, std::string> LearnCMake_VERSION(){
+    return {LearnCMake_VERSION_MAJOR, LearnCMake_VERSION_MINOR, LearnCMake_VERSION_PATCH};
+}
+
+std::string Learn::Version()
+{
+    auto [mj, mn, pt] = LearnCMake_VERSION();
+    return (mj + "." + mn + "." + pt);
+}
