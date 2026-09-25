b2875023e3f37e46fcaf6a9f93fcffc6740df191
David Tamaratare Oghenebrume <tamaratare.dave@gmail.com>
2026-09-25 21:48:58 +0100

    Update

--- src/ui/bmtlgb-src.cpp
+++ src/ui/bmtlgb-src.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
 #include <slint.h>
 #include <bmtlgb-src.h>
+#include <cstdio>
 
 auto main(void) -> int {
+    ::setenv("SLINT_APP_ID", "Bmtlgb", 1);
+
     auto app = bmtlgb::create();
     app->run();
 
